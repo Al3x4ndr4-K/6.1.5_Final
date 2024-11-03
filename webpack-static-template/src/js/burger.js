7086433de54bf7c9bb2077a88907e47c0c0fadc5
Alexandra Kiseleva <sahsakis2512@gmail.com>
2024-11-03 15:52:12 +0400

    Almost done

--- webpack-static-template/src/js/burger.js
+++ webpack-static-template/src/js/burger.js
@@ -17,20 +17,3 @@ burgerCloseButton.addEventListener('click', function () {
   burgerOverlay.classList.toggle('show-burger-overlay')
 })
 
-document.addEventListener('DOMContentLoaded', function() {
-  const burger = document.querySelector('.burger');
-
-  function handleScroll() {
-    if (window.innerWidth >= 1120) {
-      let scrollTop = window.scrollY;
-      burger.style.top = scrollTop + 'px';
-    } else {
-      burger.style.top = '';
-    }
-  }
-
-  window.addEventListener('scroll', handleScroll);
-  window.addEventListener('resize', handleScroll);
-
-  handleScroll();
-});
